685caee2b3d48352f07d44e99f3fc088a4782b02
Leonardo Pedrozo Amaral <leonardo.pedrozo@hotmail.com>
2019-10-05 00:32:28 -0300

    fix de problemas no arquivo de session

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ with open(sys.argv[3]) as file:
     code = file.read()
 
 with open('session') as file:
-    cookies = dict(c.split('=') for c in file.readline().replace(';', '').split(' '))
+    cookies = dict(c.split('=') for c in file.readline().replace(';', '').strip().split(' '))
 
 headers = {
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36 OPR/63.0.3368.94'
